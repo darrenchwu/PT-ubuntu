5cbbd2c00be88a8157787ed3053cf664e83d5019
darrenchwu <darrenchwu@gmail.com>
2018-01-17 22:28:01 +0800

    Add files via upload

--- runpt-ptf.sh
+++ runpt-ptf.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Author:  Xlemon
-# 此腳本的原始作者為 Xlemon - Telegram @xlemons
+# Contact me: Telegram @xlemons
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 clear
-
+printf "
+#############################################################################################
+#     PT Runtime environment setup for Alpharacks Debian 7&8 Mayby Support Ubunt 14-16      #
+#                 For more information please concact me via Telegram                       #
+#############################################################################################
+"
 # Check if user is root
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
 #update system
@@ -23,14 +28,14 @@ npm install pm2@latest -g
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg —dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-get install -y apt-transport-https
+apt-get install  apt-transport-https
 apt-get update
-apt-get install -y dotnet-sdk-2.1.3curl https://packages.microsoft.com/keys/microsoft.asc | gpg —dearmor > microsoft.gpg
+apt-get install  dotnet-sdk-2.1.3curl https://packages.microsoft.com/keys/microsoft.asc | gpg —dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 apt-get install apt-transport-https
 apt-get update
-apt-get install -y dotnet-sdk-2.1.3
+apt-get install  dotnet-sdk-2.1.3
 
 
 # Print java version
